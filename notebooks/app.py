8a83b6318e9ef35c15f85934db9964f2201ce43d
guiaraujofreitas <guiaraujofreitas@outlook.com>
2023-05-01 17:44:38 -0300

    primeiro commit em novo repositorio

---
+++ notebooks/app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+
+from src.extraction import load_data
+
+# layout full 
+st.set_page_config(layout='wide')
+
+
+def created_dataframe_section(teste):
+    teste = teste
+
+    return None
+
+def created_dataframe_section():
+    x= y
+
+
+
+def load_data():
+    return pd.read_csv('/home/guilherme/Documentos/repos/curso_git/projeto/data/processed/bikes_completed.csv')
+
+#function main
+def main():
+    def_raw = load_data()
+
+    st.dataframe(def_raw)
+    
+
+#if function main 
+if __name__= '__main__':
+    main()
+    
+df = load_data()
+ 
+<<<<<<< HEAD
+ def teste_function_branch():
+    x= y
+    
+=======
+ 
+>>>>>>> hot-fix
---
+++ src/test.py
@@ -0,0 +1,11 @@
+
+
+def main ():
+    print('Executando o arquivo de teste')
+
+    print('Apenas mais uma linha teste')
+    
+    if __name__ == '__main__':
+        main()
+    else:
+        print('arquivo de teste foi importado')
